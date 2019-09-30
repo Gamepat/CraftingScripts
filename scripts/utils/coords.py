de2b601001cc1742663c32b8ebd95e9460365a14
Gamepat <phyrofreaky@gmail.com>
2019-09-30 16:00:05 +0200

    Added more coordinates

--- scripts/utils/coords.py
+++ scripts/utils/coords.py
@@ -1,7 +1,23 @@
-gui = {
-  "origin":     {"x": 960,  "y": 531},
-  "output":     {"x": 1325, "y": 410},
+# storage-file for gui specific coordinates
 
-  "tempStore":  {"x": 960,  "y": 555},
+gui = {
+
+  # fixed coordinates for 1920x1080
+  "origin":       {"x": 960,  "y": 531},
+  "output":       {"x": 1325, "y": 410},
+
+  "tempStore":    {"x": 960,  "y": 555},
+
+  "shulkerEmpty": {"x": 960,  "y": 470},
+  "doubleSlot":   {"x": 1015, "y": 470},
+
+  # hotbar
+  "hot8":         {"x": 1125, "y": 730},
+  "hot9":         {"x": 1175, "y": 730},
+
+
+  # recipe specific coordinates
+  "slime1":       {"x": 550,  "y": 410},
+  "slime2":       {"x": 625,  "y": 410}
 
   }
